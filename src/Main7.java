a3af43aff6e90f8ee559d3c9a2a233b49abadb1c
minhkhiet <minhkhiet14151993@gmail.com>
2023-03-08 08:40:04 +0700

    aaaa

--- src/Main7.java
+++ src/Main7.java
@@ -4,7 +4,7 @@ public class Main7 {
 
         for (int i = 5; i >= 0; i--) {
 
-            for (int j = 1; j <= 10; j++) {
+            for (int j = 1; j <= 7; j++) {
                 System.out.print("* ");
             }
 
